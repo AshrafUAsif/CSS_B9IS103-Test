c292ecc67f46ac3a0004996465b606b030b2ca6c
AshrafUAsif <asifashraf276@gmail.com>
2024-07-23 21:59:29 +0100

    Some changes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -480,4 +480,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
